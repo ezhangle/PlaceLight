28a2cb68b83bd5b9a5465d22b8e706d534507481
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-03-26 17:20:42 +0100

    initial implementation copies fixture with wrong host

---
+++ PlaceLight/Command.cs
@@ -0,0 +1,121 @@
+#region Namespaces
+using System;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using OperationCanceledException = Autodesk.Revit.Exceptions.OperationCanceledException;
+#endregion
+
+namespace PlaceLight
+{
+  public class LightPickFilter : ISelectionFilter
+  {
+    public bool AllowElement( Element e )
+    {
+      return e.Category.Id.IntegerValue.Equals(
+        (int) BuiltInCategory.OST_LightingFixtures );
+    }
+
+    public bool AllowReference( Reference r, XYZ p )
+    {
+      return false;
+    }
+  }
+
+  [Transaction( TransactionMode.Manual )]
+  public class Command : IExternalCommand
+  {
+    static void PlaceALight( 
+      Document doc, 
+      XYZ lightPlacePoint, 
+      FamilySymbol lightSymbol )
+    {
+      // I tried every combination of numbers here and nothing worked.
+
+      XYZ bubbleEnd = new XYZ( 5, 0, 0 );
+      XYZ freeEnd = new XYZ( -5, 0, 0 );
+      XYZ thirdPt = new XYZ( 0, 0, 1 );
+
+      ReferencePlane referencePlane 
+        = doc.Create.NewReferencePlane2( bubbleEnd, 
+          freeEnd, thirdPt, doc.ActiveView );
+      
+      XYZ xAxisOfPlane = new XYZ( 0, 0, -1 );
+
+      // Error here:
+
+      doc.Create.NewFamilyInstance( 
+        referencePlane.Reference, lightPlacePoint, 
+        xAxisOfPlane, lightSymbol );
+    }
+
+    public Result Execute( 
+      ExternalCommandData commandData, 
+      ref string message, 
+      ElementSet elements )
+    {
+      var uiApp = commandData.Application;
+      var doc = uiApp.ActiveUIDocument.Document;
+
+      try
+      {
+        Selection selection = uiApp.ActiveUIDocument.Selection;
+
+        // Pick a light fixture
+
+        var pickedLightReference = selection.PickObject( 
+          ObjectType.Element, new LightPickFilter(), 
+          "Please select lighting fixture to place" );
+
+        if( pickedLightReference == null )
+        {
+          return Result.Failed;
+        }
+
+        // Get Family Instance of the selected light reference
+
+        FamilyInstance lightFamilyInstance 
+          = doc.GetElement( pickedLightReference ) 
+            as FamilyInstance;
+
+        // Get FamilySymbol of the family instance
+
+        if( lightFamilyInstance == null )
+        {
+          return Result.Failed;
+        }
+
+        FamilySymbol lightFamilySymbol 
+          = lightFamilyInstance.Symbol;
+
+        // Get new light location
+
+        XYZ placeXyzPoint = selection.PickPoint( 
+          "Select Point to place light:" );
+
+        using( var trans = new Transaction( doc ) )
+        {
+          trans.Start( "LightArray" );
+
+          // Start placing lights
+
+          PlaceALight( doc, placeXyzPoint, lightFamilySymbol );
+
+          trans.Commit();
+        }
+      }
+      catch( OperationCanceledException )
+      {
+        return Result.Cancelled;
+      }
+      catch( Exception ex )
+      {
+        message = ex.Message;
+        return Result.Failed;
+      }
+      return Result.Succeeded;
+    }
+  }
+}
